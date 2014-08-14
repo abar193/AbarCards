86294a8e24e58ce64c32a7bbd6d1cd66f7fcdfe5
Anton Bardishev <abar193@gmail.com>
2014-08-14 14:06:07 +0200

    Refractored RealPlayer to improve code logic and users experience

--- src/players/RealPlayer.java
+++ src/players/RealPlayer.java
@@ -100,56 +100,66 @@ public class RealPlayer implements PlayerInterface {
 	@Override
 	public void makeTurn() {
 		ArrayList<Unit> myArmy = latestSituation.playerUnits.get(me.playerNumber);
-		ArrayList<Unit> hisArmy = latestSituation.playerUnits.get(opponent.playerNumber);
+		//ArrayList<Unit> hisArmy = latestSituation.playerUnits.get(opponent.playerNumber);
 		
 		int c = 0;
 		boolean targeting = false;
 		int selectedUnit = 0;
-		while(c != 122) { // z key
+		while(c != 'z') { 
 			try {
 				c = System.in.read();
-				if((c >= 48 && c <= 57) || c == '-') {
-					int i = c - 48;
-					if(!targeting) {
-						if (myArmy.size() > i) {
-							reciveAction("Select target: ");
-							targeting = true;
-							selectedUnit = i;
-						}
-					} else {
-						targeting = false;
-						if(c == '-') {
-							if(parent.attackIsValid(selectedUnit, -1, me.playerNumber, 
-									opponent.playerNumber)) 
-							{
-								parent.commitAttack(selectedUnit, -1, me.playerNumber, 
-										opponent.playerNumber);
+				if(!targeting) {
+					switch(c) {
+						case '0': case '1': case '2': case '3':
+						case '4': case '5': case '6': case '7':
+						case '8': case '9': 
+							int i = c - '0';
+							if(myArmy.size() > i && myArmy.get(i).canAttack()) {
+								reciveAction("Select target: ");
+								targeting = true;
+								selectedUnit = i;
 							} else {
-								reciveAction("Invalid action");
+								reciveAction("Invalid unit selected");
 							}
-							
-						} else if(hisArmy.size() > i) {
-							if(parent.attackIsValid(selectedUnit, i, 
-									me.playerNumber, opponent.playerNumber)) 
-							{
-								parent.commitAttack(selectedUnit, i, 
-									me.playerNumber, opponent.playerNumber);
-							} else {
-								reciveAction("Invalid action");
-							}
-						}
-					}
-				} else {
-					for(int i = 0; i < qwerty.length; i++) {
-						if(c == qwerty[i]) {
-							if(me.getHand().size() > i) {
-								if(parent.canPlayCard(me.getHand().get(i), me.playerNumber)) {
-									parent.playCard(me.getHand().get(i), me.playerNumber);
-								} else {
-									reciveAction("Can't play that");
+							break;
+						case 'q': case 'w': case 'e': case 'r': case 't':
+						case 'a': case 's': case 'd': case 'f': case 'g':
+							for(int j = 0; j < qwerty.length; j++) {
+								if(c == qwerty[j]) {
+									if(me.getHand().size() > j) {
+										if(parent.canPlayCard(me.getHand().get(j), me.playerNumber)) {
+											parent.playCard(me.getHand().get(j), me.playerNumber);
+										} else {
+											reciveAction("Can't play that");
+										}
+									}
 								}
 							}
-						}
+							break;
+						case 10: case 13: break;
+						default: break;
+					}
+				} else {
+					switch(c) {
+						case '0': case '1': case '2': case '3':
+						case '4': case '5': case '6': case '7':
+						case '8': case '9': case '-': 
+							targeting = false;
+							
+							int tu, tp = opponent.playerNumber;
+							tu = (c == '-') ? -1 : c - '0';
+							
+							if(parent.attackIsValid(selectedUnit, tu, me.playerNumber, tp)) {
+								parent.commitAttack(selectedUnit, tu, me.playerNumber, tp);
+							} else {
+								reciveAction("Invalid target");
+							}
+							break;
+						case 10: case 13: 
+							break;
+						default: 
+							targeting = false; 
+							break;
 					}
 				}
 			} catch (java.io.IOException e) {
