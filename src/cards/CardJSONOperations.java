1a7fd9bc9a5716d097213a3cde02723302f52d12
Anton Bardishev <abar193@gmail.com>
2014-09-22 14:45:51 +0300

    Add CardJSONOperations

---
+++ src/cards/CardJSONOperations.java
@@ -0,0 +1,53 @@
+package cards;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.json.*;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+
+import decks.DeckPackReader;
+
+public class CardJSONOperations {
+
+	private static final String[] names = {"Neutrals", "Machines"};
+	private static final String[] links = {"NeutralsDeck.xml", "MachinesDeck.xml"};
+	
+	public BasicCard cardFromMap(Map<String, String> m) {
+		try { 
+			String name = m.get("Name");
+			int cost = Integer.parseInt(m.get("Cost"));
+			int deck = Integer.parseInt(m.get("Deck"));
+			ArrayList<BasicCard> myDeck = singleAllDeck().get(deck);
+			java.util.Iterator<BasicCard> i = myDeck.iterator();
+			while(i.hasNext()) {
+				BasicCard c = i.next();
+				if(c.name.equals(name)) {
+					if(c.cost == cost) 
+						return c;
+					else {
+						System.err.format("Card %s has wrong cost. Expected %d, got %d", name, c.cost, cost);
+					}
+				}
+			}
+		} catch (NumberFormatException | NullPointerException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	
+	static ArrayList<ArrayList<BasicCard>> allDecks = null;
+	public static synchronized ArrayList<ArrayList<BasicCard>> singleAllDeck() {
+		if(allDecks == null) {
+			allDecks = new ArrayList<ArrayList<BasicCard>>(links.length);
+			DeckPackReader dpr = new DeckPackReader();
+			for(String s : links) {
+				allDecks.add(dpr.parseFile(s));
+			}
+		}
+		
+		return allDecks;
+	}
+}
